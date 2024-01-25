cf3fcb0de4c9849c4357d97ab40217896b5e13f0
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-01-25 11:07:47 +0300

    Testing Fundamentals
    with MSUnit and NUnit

---
+++ TestingWorkshop.NUnitTest/CalculatorNUnitTests.cs
@@ -0,0 +1,117 @@
+﻿namespace TestingWorkshop.NUnitTest;
+
+[TestFixture]
+public class CalculatorNUnitTests
+{
+    [Test]
+    public void AddNumbers_InputTwoInt_GetCorrectAddition()
+    {
+        // Arrange
+        Calculator calculator = new Calculator();
+
+        // Act
+        int result = calculator.AddNumbers(10, 20);
+
+        // Assert
+        Assert.AreEqual(30, result);
+    }
+
+    [Test]
+    public void SubstractNumbers_InputTwoInt_GetCorrectSubstraction()
+    {
+        // Arrange
+        Calculator calculator = new Calculator();
+
+        // Act
+        int result = calculator.SubstractNumbers(50, 10);
+
+        // Assert
+        Assert.AreEqual(40, result);
+    }
+
+    [Test]
+    public void MultiplyNumbers_InputTwoInt_GetCorrectMultiplication()
+    {
+        // Arrange
+        Calculator calculator = new Calculator();
+
+        // Act
+        int result = calculator.MultiplyNumbers(2, 3);
+
+        // Assert
+        Assert.AreEqual(6, result);
+    }
+
+    [Test]
+    public void DivideNumbers_InputTwoInt_GetCorrectDivision()
+    {
+        // Arrange
+        Calculator calculator = new Calculator();
+
+        // Act
+        int result = calculator.DivideNumbers(50, 10);
+
+        // Assert
+        Assert.AreEqual(5, result);
+    }
+
+    [Test]
+    public void IsNumberOdd_InputOddInt_ReturnTrue()
+    {
+        // Arrange
+        Calculator calculator = new Calculator();
+
+        // Act
+        bool oddResult = calculator.IsNumberOdd(1);
+
+        // Assert
+        Assert.IsTrue(oddResult);
+        //Assert.That(oddResult, Is.True);
+        //Assert.That(oddResult, Is.EqualTo(true));
+    }
+
+    [Test]
+    public void IsNumberOdd_InputEvenInt_ReturnFalse()
+    {
+        // Arrange
+        Calculator calculator = new Calculator();
+
+        // Act
+        bool evenResult = calculator.IsNumberOdd(2);
+
+        // Assert
+        Assert.IsFalse(evenResult);
+        //Assert.That(evenResult, Is.False);
+        //Assert.That(evenResult, Is.EqualTo(false));
+    }
+
+    [Test]
+    public void IsNumberEven_InputOddInt_ReturnFalse()
+    {
+        // Arrange
+        Calculator calculator = new Calculator();
+
+        // Act
+        bool oddResult = calculator.IsNumberEven(1);
+
+        // Assert
+        Assert.IsFalse(oddResult);
+        //Assert.That(oddResult, Is.False);
+        //Assert.That(oddResult, Is.EqualTo(false));
+    }
+
+    [Test]
+    public void IsNumberEven_InputEvenInt_ReturnTrue()
+    {
+        // Arrange
+        Calculator calculator = new Calculator();
+
+        // Act
+        bool evenResult = calculator.IsNumberEven(2);
+
+        // Assert
+        Assert.True(evenResult);
+        //Assert.That(evenResult, Is.True);
+        //Assert.That(evenResult, Is.EqualTo(true));
+    }
+}
---
+++ TestingWorkshop/Calculator.cs
@@ -0,0 +1,34 @@
+﻿namespace TestingWorkshop;
+
+public class Calculator
+{
+    public int AddNumbers(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public int SubstractNumbers(int num1, int num2)
+    {
+        return num1 - num2;
+    }
+
+    public int MultiplyNumbers(int num1, int num2)
+    {
+        return num1 * num2;
+    }
+
+    public int DivideNumbers(int num1, int num2)
+    {
+        return num1 / num2;
+    }
+
+    public bool IsNumberOdd(int num)
+    {
+        return num % 2 != 0;
+    }
+
+    public bool IsNumberEven(int num)
+    {
+        return num % 2 == 0;
+    }
+}
